0e9ddcba9e96f07766cf3e81420f8d743e183e2b
Anil Maurya <anil2711@hotmail.com>
2022-01-10 22:12:34 +0530

    Person class Added

---
+++ WebBased/MiscProjectsWeb/FnFManagement/Models/Person.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FnFManagement.Models
+{
+    public class Person
+    {
+        [Key]
+        public int PersionId { get; set; }
+        [Required]
+        [StringLength(15)]
+        public string NameFirst { get; set; }
+        [StringLength(5)]
+        public string NameMiddle { get; set; }
+                
+        [Required]
+        [StringLength(20)]
+        public string NameLast { get; set; }
+
+        [StringLength(25)]
+        public string MobileNumber { get; set; }
+
+        [StringLength(30)]
+        public string AdhaarNumber { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string EmailId { get; set; }
+
+
+    }
+}

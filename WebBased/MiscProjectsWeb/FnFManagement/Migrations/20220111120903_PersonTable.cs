8e5af0ec0f265802b44b89f859e0abe4d7a2827e
Anil Maurya <anil2711@hotmail.com>
2022-01-11 17:42:47 +0530

    Adding changes

---
+++ WebBased/MiscProjectsWeb/FnFManagement/Migrations/20220111120903_PersonTable.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FnFManagement.Migrations
+{
+    public partial class PersonTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    PersionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NameFirst = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    NameMiddle = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: false),
+                    NameLast = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    MobileNumber = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    AdhaarNumber = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    EmailId = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    PersonType = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.PersionId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "People");
+        }
+    }
+}

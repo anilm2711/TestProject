f0d1cd24f03f40bf13cd3cffc1fa385f5ae3db9e
Anil Maurya <anil2711@hotmail.com>
2022-01-23 17:58:41 +0530

    Adding RazorLibrary

---
+++ WebBased/BlazorAppServer/Pages/EditActorBase.cs
@@ -0,0 +1,27 @@
+﻿using BlazorAppServer.Services;
+using EBazarModels.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorAppServer.Pages
+{
+    public class EditActorBase:ComponentBase
+    {
+        [Inject]
+        public IActorService service { get; set; }
+        public Actor Actor { get; set; } = new Actor();
+
+        [Parameter]
+        public string Id { get; set; }
+
+        [Parameter]
+        public string picUrl { get; set; } = string.Empty;
+
+        protected override async Task OnInitializedAsync()
+        {
+            Id = Id ?? "1";
+            Actor =await service.GetResultByIdAsync($"api/Actors/{Id}");
+            picUrl = Actor.ProfilePictureURL;
+        }
+
+    }
+}

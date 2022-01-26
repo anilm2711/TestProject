ceed9624f4b33511bb9a517d7998de44ded735ef
Anil Maurya <anil2711@hotmail.com>
2022-01-26 16:36:03 +0530

    Added

--- WebBased/AKM.Component/CustomeValidators/ConfirmBase.cs
+++ WebBased/AKM.Component/CustomeValidators/ConfirmBase.cs
@@ -9,5 +9,28 @@ namespace AKM.Component.CustomeValidators
 {
     public class ConfirmBase:ComponentBase
     {
+        [Parameter]
+        public bool ShowConfirmation { get; set; } = false;
+
+        [Parameter]
+        public string ConfirmationTitle { get; set; } = "Confirm Delete";
+
+        [Parameter]
+        public string ConfirmationMessage { get; set; } = "Are you sure you want to delete";
+
+        [Parameter]
+        public EventCallback<bool> ConfirmationChanged { get; set; }
+
+        public void Show()
+        {
+            ShowConfirmation = true;
+            StateHasChanged();
+        }
+        protected async Task OnConfirmationChange(bool result)
+        {
+            ShowConfirmation = false;
+            ConfirmationChanged.InvokeAsync(result);
+        }
+
     }
 }

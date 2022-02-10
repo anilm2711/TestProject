c1161cdc6335344aac7572f6cd6ba5bdc480d704
Anil Maurya <anil2711@hotmail.com>
2022-02-10 23:02:52 +0530

    addin order

---
+++ WebBased/BlazorAppServer/Pages/Orders/OrderHome.cs
@@ -0,0 +1,22 @@
+﻿using BlazorAppServer.Services;
+using EBazarModels.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorAppServer.Pages.Orders
+{
+    public partial class OrderHome:ComponentBase
+    {
+        [Inject]
+        public IOrdersService _ordersService { get; set; }
+
+        public List<Order> OrderList { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            string userId = "";// User.FindFirstValue(ClaimTypes.NameIdentifier);
+            string userRole = "";// User.FindFirstValue(ClaimTypes.Role);
+
+            var orders = await _ordersService.GetOrdersByUserIdAndRoleAsync(userId, userRole);
+        }
+    }
+}
